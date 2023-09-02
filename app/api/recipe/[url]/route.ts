291f03c2d4554a98636fc7d8363b736ed3161968
keleleo <keleleo1k@gmail.com>
2023-09-02 12:10:49 -0300

    Removed unused files

--- app/api/recipe/[url]/route.ts
+++
@@ -1,7 +0,0 @@
-import { useSearchParams } from 'next/navigation';
-import { NextResponse } from 'next/server';
-
-export async function GET(request: Request, {params:{url}}:{params:{url:string}}) {
-  console.log(url);
-  return NextResponse.json({id:0})
-}
